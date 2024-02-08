baff944040b02efdc391b5e5daa4658d55ad3037
Linclerx <justinmarx1612@icloud.com>
2024-02-08 09:21:50 +0100

    useless commit like felix

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and whalecum!");
 
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
